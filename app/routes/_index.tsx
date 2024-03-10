af387e19d4772bb9b71170e212303a5785b0f918
imgopaal <gopaldax@gmail.com>
2024-03-10 20:08:19 +0500

    first ever commit

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,41 +1,21 @@
-import type { MetaFunction } from "@remix-run/node";
+import type { MetaFunction } from '@remix-run/node'
 
 export const meta: MetaFunction = () => {
-  return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
-  ];
-};
+	return [{ title: 'New Remix App' }, { name: 'description', content: 'Welcome to Remix!' }]
+}
 
 export default function Index() {
-  return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
-  );
+	return (
+		<div className="diff h-screen">
+			<div className="diff-item-1">
+				<div className="bg-primary text-primary-content text-9xl font-black grid place-content-center">
+					Discording
+				</div>
+			</div>
+			<div className="diff-item-2">
+				<div className="bg-base-200 text-9xl font-black grid place-content-center">Discording</div>
+			</div>
+			<div className="diff-resizer"></div>
+		</div>
+	)
 }
