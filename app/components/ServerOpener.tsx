ebb6f9b843b3f6fcd0aff2070085422eac4fb2e8
imgopaal <gopaldax@gmail.com>
2024-03-12 23:05:04 +0500

    feat: refactored navbar code

---
+++ app/components/ServerOpener.tsx
@@ -0,0 +1,38 @@
+import { Link, useMatches } from '@remix-run/react'
+import { pathChecker } from '~/functions'
+
+function ServerOpener({ server }: { server: Server }) {
+	const matches = useMatches()
+
+	const { id, initials, imageUrl, notif }: Server = server
+	const currentPath = pathChecker(id, matches)
+	return (
+		<div className="relative w-full">
+			<Link
+				to={`server/${id}`}
+				className={`${currentPath ? `bg-primary rounded-[15px]` : `bg-gray-300 rounded-full`}
+								peer max-w-[48px] m-auto min-w-[48px] min-h-[48px] cursor-pointer transition group ease-out ${
+									imageUrl ? '' : `hover:bg-primary`
+								} hover:rounded-[16px] grid place-items-center font-normal text-xl capitalize active:translate-y-[2px] active:shadow-md text-white transition-transform transform ease-in-out`}
+			>
+				{imageUrl ? (
+					<img
+						src={imageUrl}
+						alt="server"
+						className={`w-[48px] h-[48px] group-hover:rounded-[16px] rounded-full object-cover
+										 ${currentPath ? `rounded-[16px]` : `rounded-full`}`}
+					/>
+				) : (
+					initials
+				)}
+			</Link>
+			<div
+				className={`w-[7px] bg-white rounded-full absolute top-1/2 bottom-1/2 transform -translate-y-1/2 left-[-3px] peer-hover:h-[20px] transition-all ease-out ${
+					currentPath ? `h-[40px] peer-hover:h-[40px]` : notif ? `h-2` : 'peer-hover:h-[20px] h-0'
+				}`}
+			></div>
+		</div>
+	)
+}
+
+export default ServerOpener
